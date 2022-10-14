675a812a01e8ed9884343ba73cc9957ca198119a
Inés Jiménez <al_ijimenezg@iessierradelasvillas.org>
2022-10-14 18:33:33 +0200

    Terminados(1-9)

--- ejercicio9.py
+++ ejercicio9.py
@@ -3,4 +3,16 @@
 num1=0
 num2=0
 num3=0
+vNumeros=[]
 
+num1=int(input("Di un número: \n"))
+num2=int(input("Di otro número: \n"))
+num3=int(input("Di un númera: \n"))
+
+vNumeros.append(num1)
+vNumeros.append(num2)
+vNumeros.append(num3)
+
+vNumeros.sort(reverse=True)
+
+print(vNumeros)
